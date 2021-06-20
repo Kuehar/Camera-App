8a60ff2b47aae14b5782725aefa376603783f8fa
kuehar <green95251@gmail.com>
2021-06-20 17:28:51 +0900

    Initial Commit

---
+++ Camera-App/Camera_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Camera_AppApp.swift
+//  Camera-App
+//
+//  Created by kuehar on 2021/06/20.
+//
+
+import SwiftUI
+
+@main
+struct Camera_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
